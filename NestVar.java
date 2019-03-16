a36a9f47f22a99bc9ceb9d34e3eb4761bb3c6311
tanzeelzaidi <tanzeelzaidi14@gmail.com>
2019-03-16 21:22:42 +0500

    Nest

---
+++ NestVar.java
@@ -0,0 +1,12 @@
+class NestVar{
+public static void main (String args[]){
+int count;
+for (count=0 ; count < 10; count= count+1)
+{
+System.out.println("this is count :" + count);
+int num;
+for (num = 0 ;num<2; num++)
+System.out.println("this program is in error!");
+}
+}
+}
