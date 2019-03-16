9d84112eb1258920e93f96acbd402750faa2f2ce
tanzeelzaidi <tanzeelzaidi14@gmail.com>
2019-03-16 21:38:25 +0500

    commit

---
+++ Example3.java
@@ -0,0 +1,14 @@
+class Example3 {
+public static void main(String args[]){
+float var;
+int x;
+var = 10;
+x =10;
+System.out.println("Original value of var:" + var );
+System.out.println("Original value of x :" +x);
+System.out.println();
+var= var/4 ;
+x = x/4;
+System.out.print("var after division :" +var);
+}
+}
