f5e865d7f5cefcbc2fcf2bfd291fae67fe510345
tanzeelzaidi <tanzeelzaidi14@gmail.com>
2019-03-16 21:31:45 +0500

    Commit

---
+++ Prime.java
@@ -0,0 +1,11 @@
+class Prime{
+public static void main (String args[]){
+int i, j;
+for(i = 2 ; i <100; i ++){
+System.out.println (i + "is prime");
+}
+for (j=2; j<= i/j ; j++){ 
+System.out.println (j + "is not prime");
+}
+}
+}
