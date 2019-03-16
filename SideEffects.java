850ca3c54a29d98f34a379c114f627dd8e1e5133
tanzeelzaidi <tanzeelzaidi14@gmail.com>
2019-03-16 21:51:17 +0500

    Side

---
+++ SideEffects.java
@@ -0,0 +1,12 @@
+class SideEffects{
+public static void main (String args[]){
+int i;
+i=0;
+if (false &(++i <100 ))
+System.out.println ("this won't be displayed");
+System.out.println ("if executed :"+ i);
+if (false &&(++i <100 ))
+System.out.println ("this won't be displayed");
+System.out.println ("if executed :"+ i);
+}
+}
