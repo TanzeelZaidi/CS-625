1f1460bbeb2d8ad89d7963578b7d5e726a00a84a
Tanzeel Zaidi <tanzeelzaidi14@gmail.com>
2019-03-27 21:26:57 +0500

    power

---
+++ Power2.java
@@ -0,0 +1,15 @@
+class Power2{
+public static void main(String args[]){
+int e;
+int result;
+for(int i=0; i < 10; i++) {
+result = 1;
+e = i;
+while(e > 0) {
+result *= 2;
+e--;
+}
+System.out.println("2 to the" + i + " power is" + result);
+}
+}
+}
