5783a12d910ca6e37a351efe2d891558e9663d0d
Tanzeel Zaidi <tanzeelzaidi14@gmail.com>
2019-05-07 18:24:55 +0500

    substring

---
+++ SubStr.java
@@ -0,0 +1,8 @@
+class SubStr {
+public static void main(String args[]) {
+String orgstr = "java makes the web move.";
+String substr = orgstr.substring(5,18);
+System.out.println("orgstr:" + orgstr);
+System.out.println("substr: " + substr);
+}
+}
